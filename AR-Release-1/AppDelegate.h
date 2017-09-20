2b1103d28dd8ccf711c8df44be0af899f6da0d14
Kumar Reddy <kumar.birapuram@myntra.com>
2017-09-20 11:42:17 +0530

    Initial Commit

---
+++ AR-Release-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AR-Release-1
+//
+//  Created by Birapuram Kumar Reddy on 9/20/17.
+//  Copyright © 2017 Myntra Design Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AR-Release-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AR-Release-1
+//
+//  Created by Birapuram Kumar Reddy on 9/20/17.
+//  Copyright © 2017 Myntra Design Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
