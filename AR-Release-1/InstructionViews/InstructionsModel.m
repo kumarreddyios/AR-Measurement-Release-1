07f986a7f09253f9088ba50252b587a3d18daf05
Mohonish Chakraborty <mohonish.chakraborty@myntra.com>
2017-10-06 17:03:59 +0530

    added storyboard view controller for permissions view

--- AR-Release-1/InstructionViews/InstructionsModel.m
+++ AR-Release-1/InstructionViews/InstructionsModel.m
@@ -14,7 +14,7 @@
     NSMutableArray *instructionsDS = [[NSMutableArray alloc] init];
     InstructionsModel *iModel1 = [[InstructionsModel alloc] init];
     iModel1.mainTitle = @"Let's get started";
-    iModel1.subTitle = @"Remove your footwear and sit comfortbly. Bend a little and point your phone towards the floor";
+    iModel1.subTitle = @"Remove your footwear and sit comfortably. Bend a little and point your phone towards the floor";
     iModel1.imageName = @"sittingIllustration";
     iModel1.buttonTitle = @"NEXT";
     iModel1.type = ARIntroduction;
@@ -22,9 +22,9 @@
 
     InstructionsModel *iModel2 = [[InstructionsModel alloc] init];
     iModel2.mainTitle = @"Surface Detection";
-    iModel2.subTitle = @"Slowly move your phone up and down till dots appear on the screenand you see a highlited area on the floor.";
+    iModel2.subTitle = @"Slowly move your phone up and down till dots appear on the screen and you see a highlited area on the floor.";
     iModel2.imageName = @"planeDetection";
-    iModel2.buttonTitle = @"TRY IT NOW";
+    iModel2.buttonTitle = @"Try Now";
     iModel2.type = ARPlane;
     [instructionsDS addObject:iModel2];
 
@@ -38,7 +38,7 @@
 
     InstructionsModel *iModel4 = [[InstructionsModel alloc] init];
     iModel4.mainTitle = @"Measure Your Foot";
-    iModel4.subTitle = @"Place your foot so that the bottom marker touches the base of your foot. Now, slide the marker to the tip of your toe. ";
+    iModel4.subTitle = @"Place your foot so that the bottom marker touches the base of your foot. Now, slide the marker to the tip of your toe.";
     iModel4.imageName = @"scaleIllusion";
     iModel4.buttonTitle = @"Try Now";
     iModel4.type = ARMeasure;
